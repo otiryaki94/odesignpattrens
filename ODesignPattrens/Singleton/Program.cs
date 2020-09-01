b39731783d04a7f7bf1b3c0a0171740f5e4e0ae8
otiryaki94 <okantiryaki@outlook.com>
2020-09-02 01:32:48 +0300

    Singleton

---
+++ ODesignPattrens/Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var productManager = ProductManager.CreateProductAsSingleton();
+            productManager.Update();
+        }
+
+        class ProductManager
+        {
+            private static ProductManager _productManager;
+            private ProductManager()
+            {
+
+            }
+
+            public static ProductManager CreateProductAsSingleton()
+            {
+                if (_productManager == null)
+                    _productManager = new ProductManager();
+
+                return _productManager;
+
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Updated");
+                Console.ReadLine();
+            }
+        }
+    }
+}
